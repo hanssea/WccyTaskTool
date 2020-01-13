0f61a04f455c6bf10c7489bdc211181c863d2afb
JinYang <jianye0209@yeah.net>
2020-01-13 16:30:34 +0800

    Initial commit

---
+++ Example/WccyTaskTool/WCCYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCCYAppDelegate.h
+//  WccyTaskTool
+//
+//  Created by hanssea on 01/13/2020.
+//  Copyright (c) 2020 hanssea. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WCCYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/WccyTaskTool/WCCYViewController.h
@@ -0,0 +1,13 @@
+//
+//  WCCYViewController.h
+//  WccyTaskTool
+//
+//  Created by hanssea on 01/13/2020.
+//  Copyright (c) 2020 hanssea. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WCCYViewController : UIViewController
+
+@end
